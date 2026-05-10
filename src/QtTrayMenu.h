58d5a4e75d4a4544f1699cf742283b75540b81f1
Kishi85 <git@eightyfive.net>
2026-05-10 11:42:27 +0200

    doc: Add doxygen comments for public functions of QtTrayMenu

--- src/QtTrayMenu.h
+++ src/QtTrayMenu.h
@@ -14,19 +14,96 @@ class QtTrayMenu: public QObject {
   Q_OBJECT
 
 public:
+  /**
+   * @brief Create a QtTrayMenu instance
+   * @param parent optional parent Qt object
+   */
   explicit QtTrayMenu(QObject *parent = nullptr);
+
+  /**
+   * @brief Create a QtTrayMenu instance
+   * @param argc argument count for QApplication (if that needs to be created)
+   * @param argv argument list for QApplication (if that needs to be created)
+   * @param debug if true isntall eventFilter for debug logging
+   * @param parent optional parent Qt object
+   */
   explicit QtTrayMenu(int argc, char **argv, bool debug, QObject *parent = nullptr);
+
   ~QtTrayMenu() override;
+  /**
+   * @brief QObject override to filter events on watched object
+   * @param watched object watched for event
+   * @param event event on object
+   * @return true if event should be filtered out and not be processed further
+   * @see https://doc.qt.io/qt-6/qobject.html#eventFilter
+   */
   bool eventFilter(QObject *watched, QEvent *event) override;
+
+  /**
+   * @brief Initialize tray with given structure
+   * @param tray struct containing tray configuration
+   * @return 0 on success
+   */
   int init(struct tray *tray);
+
+  /**
+   * @brief Update tray configuration
+   * @param tray struct containing tray configuration
+   */
   void update(struct tray *tray);
+
+  /**
+   * @brief Process tray loop events
+   * @param blocking if true the function call will block until QtTrayMenu exits
+   * @return 0 on successful processing if non-blocking, -1 otherwise
+   */
   int loop(int blocking) const;
+
+  /**
+   * @brief Initialize tray with given structure
+   */
   void exit();
+
+  /**
+   * @brief Configure metadata for QApplication
+   * @param appName the applications name
+   * @param appDisplayName the applications display name
+   * @param desktopName the applications desktop file name
+   */
   void configureAppMetadata(const QString &appName, const QString &appDisplayName, const QString &desktopName) const;
+
+  /**
+   * @brief Show tray context menu
+   */
   void showMenu() const;
+
+  /**
+   * @brief Show tray message popup
+   * @param title popup title
+   * @param msg popup message
+   * @param icon popup icon
+   * @param msecs popup display duration
+   */
   void showMessage(const QString &title, const QString &msg, QSystemTrayIcon::MessageIcon icon = QSystemTrayIcon::Information, int msecs = 10000) const;
+
+  /**
+   * @brief Show tray message popup
+   * @param title popup title
+   * @param msg popup message
+   * @param icon popup icon
+   * @param msecs popup display duration
+   */
   void showMessage(const QString &title, const QString &msg, const QIcon &icon, int msecs = 10000) const;
+
+  /**
+   * @brief Simulate click on menu item
+   * @param index Menu item index to simulate click on
+   */
   void clickMenuItem(int index) const;
+
+  /**
+   * @brief Simulate click on popup message
+   */
   void clickMessage() const;
 
 private:
@@ -40,6 +117,9 @@ private:
   struct tray_menu *getTrayMenuItem(QAction *action);
 
 signals:
+  /**
+   * @brief Signal emitted when exiting the tray
+   */
   void exitRequested();
 
 private slots:
